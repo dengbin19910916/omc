9d1d0e53d9992d94a141639dcfd1a4678d392b43
Db <dengbin19910916@163.com>
2021-08-01 22:34:25 +0800

    clean code

--- src/main/kotlin/io/xxx/omni/omc/model/common.kt
+++ src/main/kotlin/io/xxx/omni/omc/model/common.kt
@@ -6,26 +6,27 @@ import java.time.LocalDateTime
  * 平台（外部）
  */
 data class Platform(
-    var id: String?,
-    var name: String?,
-    var enabled: Boolean?,
+    var id: String,
+    var name: String,
+    var enabled: Boolean,
 )
 
 /**
  * 店铺（外部）
  */
 data class Store(
-    var id: String?,
-    var pid: String?,
+    var id: String,
+    var pid: String,
     /**
      * 平台商家id
      */
     var oid: String?,
-    var name: String?,
-    var enabled: Boolean?,
+    var name: String,
+    var enabled: Boolean,
     var appKey: String?,
     var appSecret: String?,
     var accessToken: String?,
-    var created: LocalDateTime?,
+    var created: LocalDateTime,
+
     var platform: Platform?,
 )
